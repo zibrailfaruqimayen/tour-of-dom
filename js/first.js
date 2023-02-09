8079a1cb359d16acf305fac4ac45d3a824c4d13f
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-10 01:56:19 +0600

    add

---
+++ js/first.js
@@ -0,0 +1,19 @@
+console.log('first file');
+
+
+const allLi = document.getElementsByTagName('li');
+console.log(allLi);
+
+
+const allTitle = document.getElementsByClassName('section-title');
+console.log(allTitle)
+
+const secondSection = document.getElementById('second-section');
+console.log(secondSection);
+secondSection.style.color = 'white';
+secondSection.style.backgroundColor = 'tomato';
+
+ 
+// document.querySelectorAll
+// document.querySelector
+// 
---
+++ js/second.js
@@ -0,0 +1,11 @@
+console.log('second file');
+
+
+const secondList = document.getElementById('second-list');
+
+const li = document.createElement('li');
+li.innerText = 'My Dynamic Item'
+
+secondList.appendChild(li)
+
+
